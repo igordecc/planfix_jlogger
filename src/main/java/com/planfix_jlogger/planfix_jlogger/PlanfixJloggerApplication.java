43ee9fa38a9f3749618258132aa2cf877e75e898
Игорь Десятов <DesyatovIV@gkosar.corp>
2021-05-14 10:34:08 +0400

    initial commit
    empty project

---
+++ src/main/java/com/planfix_jlogger/planfix_jlogger/PlanfixJloggerApplication.java
@@ -0,0 +1,13 @@
+package com.planfix_jlogger.planfix_jlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanfixJloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanfixJloggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planfix_jlogger/planfix_jlogger/PlanfixJloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planfix_jlogger.planfix_jlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanfixJloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
